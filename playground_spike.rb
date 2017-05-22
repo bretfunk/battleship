9fe1b7570866718b5325911e6c017d6dffaeea65
BFunk <bret.e.funk@gmail.com>
2017-05-21 19:19:22 -0600

    added two methods for computer ship position generation.

--- playground_spike.rb
+++ playground_spike.rb
@@ -2,7 +2,7 @@ class Computer
   attr_accessor :game_board, :full_game_board #for testing
   def initialize
   @game_board = {
-    "A" => {"1" => "", "2" => "", "3" => "", "4" => ""},
+    "A" => {"1" => "H", "2" => "", "3" => "", "4" => ""},
     "B" => {"1" => "", "2" => "", "3" => "", "4" => ""},
     "C" => {"1" => "", "2" => "", "3" => "", "4" => ""},
     "D" => {"1" => "", "2" => "", "3" => "", "4" => ""}
@@ -43,17 +43,20 @@ class Computer
     board[coordinate[0]][coordinate[1]].empty?
   end
 
-  def letter_next_door?(original, new_location)
-    letter_array = [original[0].ord + 1, original[0].ord - 1]
-    letter_array.any? {|letter| letter == new_location[0].ord}
+  def hit?(coordinate, board=game_board)
+    board[coordinate[0]][coordinate[1]] == "H"
   end
 
-  def number_next_door?(original, new_location)
-    num_array = [original[1].to_i + 1, original[1].to_i - 1]
-    num_array.any? {|num| num == new_location[1].to_i}
-  end
+  # def letter_next_door?(original, new_location)
+  #   letter_array = [original[0].ord + 1, original[0].ord - 1]
+  #   letter_array.any? {|letter| letter == new_location[0].ord}
+  # end
+  #
+  # def number_next_door?(original, new_location)
+  #   num_array = [original[1].to_i + 1, original[1].to_i - 1]
+  #   num_array.any? {|num| num == new_location[1].to_i}
+  # end
 
-  #not working for some reason, keeps having end issues
   def computer_shoot
     shot = random_coordinate
     if available?(shot)
@@ -64,20 +67,18 @@ class Computer
   end
 end
 
-#same problem with this as with close letters
   def array_close_numbers?(size, array)
     just_numbers = []
     array.each {|location| just_numbers << location[1].to_i}
     just_numbers = just_numbers.sort
-    just_numbers.last - just_numbers.first == size
+    just_numbers.last - just_numbers.first == size - 1
   end
 
-#"A" is 65 and "D" is 68, just subtracting one wont fix it for some reason
   def array_close_letters?(size, array)
     just_letters = []
     array.each {|location| just_letters << location[0]}
     just_letters = array.map{ |letter| letter.ord}.sort
-    just_letters.last - just_letters.first == size
+    just_letters.last - just_letters.first == size - 1
   end
 
   def array_same_num?(array)
@@ -88,33 +89,39 @@ end
     array.all? {|location| array[0][0] == location[0]}
   end
 
+  def ship_creator_filter(size, ship)
+    num = rand(1..2)
+    if num == 1 && (array_close_letters?(size, ship) && array_same_num?(ship))
+      return true
+    elsif num == 2 && (array_close_numbers?(size, ship) && array_same_letter?(ship))
+      return true
+    else
+      return false
+    end
+  end
+
   def computer_ship_creator(size)
     ship = []
     until ship.length == size
       random = random_coordinate
       ship << random unless available?(random) == false || ship.include?(random)
     end
-    #need to create a random method so the or doesn't always go one way
-    #only starting with letters
-    if array_close_letters?(size, ship) && array_same_num?(ship) || array_close_numbers?(size, ship) && array_same_letter?(ship)
-      return ship
-    else
-      computer_ship_creator(size)
-    end
+    ship_creator_filter(size, ship) ? ship : computer_ship_creator(size)
   end
 end
 
+def ship_inserter(array)
+end
+
 test = Computer.new
-puts test.computer_shoot
-#p test.computer_ship_creator(2)
-#p test.array_close_numbers?(3, ["A4", "A1", "A2", "A3"])
+puts  test.hit?("A1")
+#puts test.computer_shoot
+#p test.computer_ship_creator()
+#p test.array_close_numbers?(3, ["A1", "A2", "A3"])
 #p test.array_close_letters?(3, ["A", "D", "C", "B"])
 #puts test.array_same_num?(["A1", "B1", "C1"])
 #puts test.array_same_letter?(["A1", "A2", "A3"])
 
 #to do
-#test all methods again
-#figure out why having these errors
-#figure out shoot weird end issue
 #start writing an insert method for shoot, hit and miss
 #new method for hit or miss, return true or false
