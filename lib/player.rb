e3718f508c8a8d24f9fa27850d8960704168c6ae
BFunk <bret.e.funk@gmail.com>
2017-05-22 10:23:57 -0600

    add player methods and tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,72 @@
 class Player
-  #can the player shoot
+  attr_accessor :game_board, :full_game_board
+  def initialize
+  @game_board = {
+    "A" => {"1" => " ", "2" => " ", "3" => " ", "4" => " "},
+    "B" => {"1" => " ", "2" => " ", "3" => " ", "4" => " "},
+    "C" => {"1" => " ", "2" => " ", "3" => " ", "4" => " "},
+    "D" => {"1" => " ", "2" => " ", "3" => " ", "4" => " "}
+  }
+  @full_game_board = { #for testing
+    "A" => {"1" => "S", "2" => "S", "3" => "S", "4" => "S"},
+    "B" => {"1" => "S", "2" => "S", "3" => "S", "4" => "S"},
+    "C" => {"1" => "S", "2" => "S", "3" => "S", "4" => "S"},
+    "D" => {"1" => "S", "2" => "S", "3" => "S", "4" => "S"}
+  }
+  end
+
+  def output(board=@game_board)
+    puts "============="
+    puts "   1  2  3  4"
+    print "A"; board["A"].values.each {|x| print "  " + x}.join; print "\n\n"
+    print "B"; board["B"].values.each {|x| print "  " + x}.join; print "\n\n"
+    print "C"; board["C"].values.each {|x| print "  " + x}.join; print "\n\n"
+    print "D"; board["D"].values.each {|x| print "  " + x}.join; print "\n"
+    puts "============="
+  end
+
+  def game_board
+    @game_board
+  end
+
+  def full_game_board
+    @full_game_board
+  end
+
+  def what_is_your_name
+    puts "What is your name?"
+    name_answer = gets.chomp
+    return name_answer
+  end
+
+  def player_ship_placement
+    puts "Where do you want to place your ships?"
+    answer = gets.chomp
+  end
+
+  def player_shoot
+    puts "Where do you want to shoot?"
+    answer = gets.chomp
+  end
+
+  def available?(coordinate, board=game_board)
+    board[coordinate[0]][coordinate[1]] == " "
+  end
+
+  def ship_hit?(coordinate, board=game_board)
+    board[coordinate[0]][coordinate[1]] == "S"
+  end
+
+  def player_shoot(coordinate) #might have to change return to puts
+    if available?(coordinate)
+      if ship_hit?(coordinate)
+        return "Hit!"
+      else
+        return "Miss!"
+      end
+    else
+      return "Not a valid location"
+    end
+  end
+
 end
--- test/test_player.rb
+++ test/test_player.rb
@@ -12,6 +12,50 @@ class PlayerTest < Minitest::Test
     assert_equal false, result
   end
 
+  def test_game_board_exists
+    test = Player.new
+    result = test.game_board.nil?
+    assert_equal false, result
+  end
 
+  def test_available
+    test = Player.new
+    result = test.available?("A1")
+    assert_equal true, result
+  end
+
+  def test_ship_hit_without_ship
+    test = Player.new
+    result = test.ship_hit?("A1")
+    assert_equal false, result
+  end
+
+  def test_ship_hit_with_ship
+    test = Player.new
+    test.game_board["A"]["1"] = "S"
+    result = test.ship_hit?("A1")
+    assert_equal true, result
+  end
+
+  def test_player_shoot_with_hit #might have to change puts for return
+    test = Player.new
+    test.game_board["A"]["1"] = "S"
+    result = test.player_shoot("A1")
+    assert_equal "Hit!", result #not working
+  end
+
+  def test_player_shoot_with_miss
+    test = Player.new
+    test.game_board["A"]["1"] = " "
+    result = test.player_shoot("A1")
+    assert_equal "Miss!", result
+  end
+
+  def test_player_shoot_with_invalid_location
+    test = Player.new
+    test.game_board["A"]["1"] = "M"
+    result = test.player_shoot("A1")
+    assert_equal "Not a valid location", result
+  end
 
 end
