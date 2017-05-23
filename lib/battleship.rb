86b5075b1a7435264e608a2b8eb386cd22d4b003
BFunk <bret.e.funk@gmail.com>
2017-05-23 08:09:59 -0600

    add battleship text and instance tests

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,2 +1,35 @@
+require './lib/board'
+require './lib/computer'
+require './lib/gameplay'
+require './lib/interface'
+require './lib/player'
+
 class Battleship
+  def initialize
+    @board = Board.new
+    @computer = Computer.new
+    @gameplay = Gameplay.new
+    @interface = Interface.new
+    @player = Player.new
+  end
+
+  def board
+    @board
+  end
+
+  def computer
+    @computer
+  end
+
+  def gameplay
+    @gameplay
+  end
+
+  def interface
+    @interface
+  end
+
+  def player
+    @player
+  end
 end
--- test/test_battleship.rb
+++ test/test_battleship.rb
@@ -4,9 +4,40 @@ require 'minitest/pride'
 require './lib/battleship'
 
 class BattleshipTest < Minitest::Test
+
   def test_class_exists
     test = Battleship.new
     result = test.nil?
+    assert_equal false, result
+  end
+
+  def test_board_instance
+    test = Battleship.new
+    result = test.board.nil?
+    assert_equal false, result
+  end
+
+  def test_computer_instance
+    test = Battleship.new
+    result = test.computer.nil?
+    assert_equal false, result
+  end
+
+  def test_gameplay_instance
+    test = Battleship.new
+    result = test.gameplay.nil?
+    assert_equal false, result
+  end
+
+  def test_interface_instance
+    test = Battleship.new
+    result = test.interface.nil?
+    assert_equal false, result
+  end
+
+  def test_player_instance
+    test = Battleship.new
+    result = test.player.nil?
     assert_equal false, result
   end
 
