1ddad091eb6b894163d2dc0ac1da7f8fc79bef83
BFunk <bret.e.funk@gmail.com>
2017-05-23 06:51:38 -0600

    add visual output for ship game board for testing purposes and possible cheat code.

--- lib/board.rb
+++ lib/board.rb
@@ -33,4 +33,14 @@ class Board
       print "D"; board["D"].values.each {|x| print "  " + x}.join; print "\n"
       puts "============="
     end
+
+    def ship_output(board=@ship_board) #for testing
+      puts "============="
+      puts "   1  2  3  4"
+      print "A"; board["A"].values.each {|x| print "  " + x}.join; print "\n\n"
+      print "B"; board["B"].values.each {|x| print "  " + x}.join; print "\n\n"
+      print "C"; board["C"].values.each {|x| print "  " + x}.join; print "\n\n"
+      print "D"; board["D"].values.each {|x| print "  " + x}.join; print "\n"
+      puts "============="
+    end
 end
