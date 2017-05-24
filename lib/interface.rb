eadd668c11c65d2fac9a9423f5f5a360c3568897
BFunk <bret.e.funk@gmail.com>
2017-05-24 12:59:00 -0600

    add interface messages

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,58 +1,78 @@
 module Interface
 
-#   def what_is_your_name
-#     puts "What is your name?"
-#     name_answer = gets.chomp
-#     return name_answer
-#   end
-#
-#   def welcome_message
-#     "Welcome to BATTLESHIP\n\n\n Would you like to (p)lay, read the (i)nstructions, or (q)uit?\n\n>"
-#   end
-#
-#   def computer_ship_placement
-#     "I have laid out my ships on the grid.\n
-#     You now need to layout your two ships.\n
-#     The first is two units long and the\nsecond is three units long.\n
-#     The grid has A1 at the top left and D4 at the bottom right.\n
-#     Enter the squares for the two-unit ship:"
-#   end
-#
-#   def player_ship_placement
-#     puts "Where do you want to place your ships?"
-#     answer = gets.chomp
-#   end
-#
-#   def player_shoot
-#     puts "Where do you want to fire?"
-#     answer = gets.chomp
-#   end
-#
-#   def hit
-#     "You hit an enemy ship!"
-#   end
-#
-# #create
-#   def sunk
-#   # "You sunk a #{size} ship!"
-#   end
-#
-#   def sorry
-#     "Sorry, the computer won!"
-#   end
-#
-#   def winner
-#     "You won!"
-#   end
-#
-# #create
-#   def shots_fired
-#   #  "#{winner_shots_counter}"
-#   end
-#
-# #create
-#   def time_alloted
-#     # "time since start #{time}"
-#   end
+  def message_what_is_your_name
+    puts "What is your name?"
+    name_answer = gets.chomp
+    return name_answer
+  end
+
+  def message_welcome
+    "Welcome to BATTLESHIP\n\n Would you like to (p)lay, read the (i)nstructions, or (q)uit?\n>"
+  end
+
+  def message_welcome_choice(choice)
+    case choice
+    when "p" then puts play_game
+    when "i" then puts message_instructions
+    when "q" then puts message_exit
+    end
+  end
+
+  def play_game
+  end
+
+  def message_computer_ship_placement
+    "I have laid out my ships on the grid.\n
+    You now need to layout your two ships.\n
+    The first is two units long and the\nsecond is three units long.\n
+    The grid has A1 at the top left and D4 at the bottom right.\n
+    Enter the squares for the two-unit ship:"
+  end
+
+  def message_player_ship_placement
+    puts "Where do you want to place your ships?"
+    answer = gets.chomp
+  end
+
+  def message_player_shoot
+    puts "Where do you want to fire?"
+    answer = gets.chomp
+  end
+
+  def message_player_hit
+    "You hit an enemy ship!"
+  end
+
+#create
+  def message_player_sunk
+  # "You sunk a #{size} ship!"
+  end
+
+  def message_sorry
+    "Sorry, the computer won!"
+  end
+
+  def message_winner
+    "You won!"
+  end
+
+#create
+  def message_shots_fired
+  #  "#{winner_shots_counter}"
+  end
+
+#create
+  def message_time_alloted
+    # "time since start #{time}"
+  end
+
+  def message_instructions
+    return "This game is called battleship.  The player places \ntwo ships on a 4x4 board, as does a computer opponent, \nand you shoot board corrdinates to sink your opponent's battleship.  \nWhomever destroys their opponent's ships first wins!\n"
+
+  end
+
+  def message_exit
+    return "Thank you for playing!"
+  end
 
 end
--- lib/runner.rb
+++ lib/runner.rb
@@ -2,30 +2,22 @@ require './lib/battleship'
 require './lib/interface'
 require './lib/gameplay'
 
+include Interface
+include Gameplay
+
 new_game = Battleship.new
-new_game.computer_new_ship(3)
-new_game.computer_new_ship(3)
-new_game.computer_new_ship(3)
-new_game.computer_new_ship(2)
-new_game.computer_new_ship(2)
-puts new_game.player_shoot("A1")
-puts new_game.player_shoot("B1")
-puts new_game.player_shoot("C1")
-puts new_game.player_shoot("D1")
-puts new_game.player_shoot("A2")
-puts new_game.player_shoot("B2")
-puts new_game.player_shoot("C2")
-puts new_game.player_shoot("D2")
-puts new_game.player_shoot("A3")
-puts new_game.player_shoot("B3")
-puts new_game.player_shoot("C3")
-puts new_game.player_shoot("D3")
-puts new_game.player_shoot("A4")
-puts new_game.player_shoot("B4")
-puts new_game.player_shoot("C4")
-puts new_game.player_shoot("D4")
-new_game.player_output
-p new_game.player_shots
-p new_game.player_hits
-p new_game.computer_shots
-p new_game.computer_hits
+puts message_welcome
+puts ">"
+welcome_answer = gets.chomp
+new_game.message_welcome_choice(welcome_answer)
+puts ">"
+
+
+#new_game.computer_new_ship(3)
+#new_game.computer_new_ship(2)
+
+#puts new_game.player_shoot("A1")
+
+
+#needs to load every time
+# new_game.player_output
